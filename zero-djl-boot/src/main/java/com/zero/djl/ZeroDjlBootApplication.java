93978f335070b2e980ffd25ee7fde9a676cfda6a
Micro-Organism <coolrabbit1993@gmail.com>
2024-11-27 11:26:54 +0800

    feat: springboot 集成 djl

---
+++ zero-djl-boot/src/main/java/com/zero/djl/ZeroDjlBootApplication.java
@@ -0,0 +1,13 @@
+package com.zero.djl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroDjlBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZeroDjlBootApplication.class, args);
+    }
+
+}
